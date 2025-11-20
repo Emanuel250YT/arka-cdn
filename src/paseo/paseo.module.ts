c36b30c64b3c05ed8f537a40767e1028666441df
Emanuel250YT <emanuel250gameryt@gmail.com>
2025-11-19 21:02:19 -0300

    refactor: remove Paseo-related files and controller logic

--- src/paseo/paseo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaseoService } from './paseo.service';
-import { PaseoController } from './paseo.controller';
-
-@Module({
-  controllers: [PaseoController],
-  providers: [PaseoService],
-})
-export class PaseoModule { }
